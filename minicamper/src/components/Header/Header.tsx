cbda3e9a22b16a29e85225602db68fd73978bf6f
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2023-09-21 01:31:30 +0300

    Phone call in header

--- minicamper/src/components/Header/Header.tsx
+++ minicamper/src/components/Header/Header.tsx
@@ -4,22 +4,23 @@ import Navigation from '../Navigation/Navigation';
 import { TNavLink } from '../Navigation/NavigationLink/NavigationLink';
 import styles from './Header.module.sass';
 
-interface HeaderProps {}
+interface HeaderProps {
+}
 
 const navLinks: TNavLink[] = [
-    { title: 'Аренда', url: ROUTE.rent },
-    { title: 'Условия аренды', url: ROUTE.conditions },
-    { title: 'Правила аренды', url: ROUTE.rules },
-    { title: 'Галерея', url: ROUTE.gallery },
-    { title: 'Контакты', url: ROUTE.contacts }
-]
+    {title: 'Аренда', url: ROUTE.rent},
+    {title: 'Условия аренды', url: ROUTE.conditions},
+    {title: 'Правила аренды', url: ROUTE.rules},
+    {title: 'Галерея', url: ROUTE.gallery},
+    {title: 'Контакты', url: ROUTE.contacts}
+];
 
 const Header: FC<HeaderProps> = () => (
-  <header className={styles.Header}>
-    <img className={styles.Logo} src="logo.png" alt="логотип" />
-    <Navigation links={navLinks}/>
-    <div>+375 (44) 700-76-55</div>
-  </header>
+    <header className={styles.Header}>
+        <img className={styles.Logo} src="logo.png" alt="логотип"/>
+        <Navigation links={navLinks}/>
+        <a href="tel:+375447007655">+375 (44) 700-76-55</ a>
+    </header>
 );
 
 export default Header;
