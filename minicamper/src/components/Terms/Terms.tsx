50fcd538ac9e2f00de95308c3d5d85e2a306857e
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2023-09-21 23:31:55 +0300

    Filled Terms section

--- minicamper/src/components/Terms/Terms.tsx
+++ minicamper/src/components/Terms/Terms.tsx
@@ -1,12 +1,63 @@
 import React, { FC } from 'react';
+import {
+    DAILY_PRICE,
+    KM_PRICE,
+    MAX_DISTANCE,
+    MIN_DRIVING_EXPERIENCE,
+    MIN_TERM,
+    PLEDGE,
+    SERVICE_FEE
+} from '../../constants/price';
 import styles from './Terms.module.sass';
 
-interface TermsProps {}
+const terms = [
+    {
+        title: <div><span className={styles.Attentive}>Стаж вождения</span> от {MIN_DRIVING_EXPERIENCE} лет</div>,
+    },
+    {
+        title: <div><span className={styles.Attentive}>Сервисный сбор</span>{SERVICE_FEE} руб.</div>,
+        description: 'В свервисный сбор входит: уборка, мойка кемпера внутри и снаружи, заправка питьевой воды, заправка газа, заряд аккумулятора.',
+    },
+    {
+        title: <div><span className={styles.Attentive}>Залог</span>{PLEDGE} руб.</div>,
+        description: 'Возвращается при сдаче кемпера без происшествий.',
+    },
+    {
+        title: <div><span className={styles.Attentive}>Сдача в аренду осуществляется</span> от {MIN_TERM} суток.</div>,
+    },
+    {
+        title: <div>Заключение <span className={styles.Attentive}>договора</span> аренды.</div>,
+    },
+];
+
+interface TermsProps {
+}
 
 const Terms: FC<TermsProps> = () => (
-  <div className={styles.Terms}>
-    Terms Component
-  </div>
+    <section className={styles.Terms}>
+        <div className={styles.Info}>
+            <h2 className={styles.Title}>Условия аренды кемпера</h2>
+            <div className={styles.TermsList}>{terms.map(({title, description}) => (
+                <div className={styles.Term}>
+                    {title}
+                    <div>{description}</div>
+                </div>
+            ))}</div>
+            <div className={styles.Prices}>
+                <div className={styles.PriceItem}>
+                    <div className={styles.Price}>{DAILY_PRICE} руб./сутки</div>
+                    от {MIN_TERM} суток и более
+                </div>
+                <div className={styles.PriceItem}>
+                    <div className={styles.Price}>1 км = {KM_PRICE} руб.</div>
+                    Доставка по Минску и в пределах {MAX_DISTANCE} км от Минска
+                </div>
+            </div>
+        </div>
+        <div className={styles.Picture}>
+            <img src='terms-picture.png' alt='песик в кемпере' />
+        </div>
+    </section>
 );
 
 export default Terms;
---
+++ minicamper/src/constants/price.ts
@@ -0,0 +1,26 @@
+/**
+ * Сервисный сбор
+ */
+export const SERVICE_FEE = 15;
+
+/**
+ * Залог
+ */
+export const PLEDGE = 300;
+/**
+ * Минимальный срок
+ */
+export const MIN_TERM = 3;
+/**
+ * Минимальный опыт вождения
+ */
+export const MIN_DRIVING_EXPERIENCE = 3;
+/**
+ * Стоимость за сутки
+ */
+export const DAILY_PRICE = 100
+/**
+ * Стоимость за километр
+ */
+export const KM_PRICE = 1
+export const MAX_DISTANCE = 100
