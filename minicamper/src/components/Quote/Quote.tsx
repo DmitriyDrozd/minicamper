2e5f135a09a68defd0758cc5785d4b1d4af50bfb
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2023-09-30 21:31:11 +0300

    Quote styled

--- minicamper/src/components/Quote/Quote.tsx
+++ minicamper/src/components/Quote/Quote.tsx
@@ -1,14 +1,17 @@
 import React, { FC } from 'react';
 import styles from './Quote.module.sass';
 
-interface QuoteProps {}
+interface QuoteProps {
+}
 
 const Quote: FC<QuoteProps> = () => (
-  <div className={styles.Quote}>
-      <div>Посмотри на мир!</div>
-      <div>он куда удивительнее снов.</div>
-      <div>-Рэй Брэдберри</div>
-  </div>
+    <div className={styles.QuoteWrapper}>
+        <div className={styles.Quote}>
+            <div className={styles.Rotated}>Посмотри на мир!</div>
+            <span className={styles.Straight}>он куда удивительнее снов.</span>
+            <span className={styles.Author}>-Рэй Брэдберри</span>
+        </div>
+    </div>
 );
 
 export default Quote;
