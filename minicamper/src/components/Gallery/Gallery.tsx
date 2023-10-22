4261d82b29e835b50ee4f43da9d0708f0f577c47
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2023-10-23 01:43:38 +0300

    Новые картинки

--- minicamper/src/components/Gallery/Gallery.tsx
+++ minicamper/src/components/Gallery/Gallery.tsx
@@ -8,7 +8,17 @@ import styles from './Gallery.module.sass';
 interface GalleryProps {}
 
 const path = 'gallery/';
-const images = ['1.jpg'];
+const images = [
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+];
 
 const Gallery: FC<GalleryProps> = () => {
     const [isImgLoaded, setIsImgLoaded] = useState(false);
