835b9362486b15ca34aa7aed7ee3c7b8d9f71c4a
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2023-10-23 00:30:31 +0300

    Оптимизирована основная картинка

--- minicamper/src/components/Greetings/Greetings.tsx
+++ minicamper/src/components/Greetings/Greetings.tsx
@@ -1,17 +1,22 @@
 import React, { FC } from 'react';
+import { isMobile } from '../../utils/device';
 import styles from './Greetings.module.sass';
 
 interface GreetingsProps {
 }
 
-const Greetings: FC<GreetingsProps> = () => (
-    <div className={styles.Greetings}>
-        <img className={styles.Picture} src="main.jpg" alt='кемпер'/>
-        <div className={styles.GreetingsText}>
-            <div className={styles.Rent}>Аренда автодома для настоящих искателей путешествий</div>
-            <div className={styles.Travel}><div className={styles.Description}>Путешествуйте</div><div className={styles.Title}>без границ</div></div>
+const Greetings: FC<GreetingsProps> = () => {
+    const photoPath = isMobile() ? 'main-mobile.jpg' : 'main.jpg';
+
+    return (
+        <div className={styles.Greetings}>
+            <img className={styles.Picture} src={photoPath} alt='кемпер'/>
+            <div className={styles.GreetingsText}>
+                <div className={styles.Rent}>Аренда автодома для настоящих искателей путешествий</div>
+                <div className={styles.Travel}><div className={styles.Description}>Путешествуйте</div><div className={styles.Title}>без границ</div></div>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Greetings;
