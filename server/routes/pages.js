30525531727c48bd9d1548f0051fce75ee594013
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2023-11-13 22:12:41 +0300

    добавлена отдача файлов по пути политики

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 /* GET landing */
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public_html', 'index.html'));
+    res.sendFile('index.html', { root: path.join(__dirname, '..', 'public_html') });
 });
 
 /* GET politics */
 router.get('/politicnda', function (req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public_html', 'index.html'));
+    res.sendFile('index.html', { root: path.join(__dirname, '..',  'public_html') });
 });
 
 module.exports = router;
