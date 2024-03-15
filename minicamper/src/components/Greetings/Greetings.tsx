e513b74c39bde7f2a7686b802a3573353f989bc0
dzmitryi.drozd <drozd.dzmitryi@gmail.com>
2024-03-15 14:01:01 +0300

    отображение полного изображения по клику главной картинки

--- minicamper/src/components/Greetings/Greetings.tsx
+++ minicamper/src/components/Greetings/Greetings.tsx
@@ -1,4 +1,9 @@
-import React, { FC } from 'react';
+import { Modal } from '@mui/material';
+import Box from '@mui/material/Box';
+import React, {
+    FC,
+    useState
+} from 'react';
 import { RoadIcon } from '../../icons/Road';
 import ContentWrapper from '../../layout/ContentWrapper/ContentWrapper';
 import { isMobile } from '../../utils/device';
@@ -9,6 +14,9 @@ interface GreetingsProps {
 
 const Greetings: FC<GreetingsProps> = () => {
     const photoPath = isMobile() ? 'main-mobile.jpg' : 'main.jpg';
+    const [isFullScreenImage, setIsFullScreenImage] = useState<boolean>(false);
+    const handleCloseFullScreen = () => setIsFullScreenImage(false)
+    const handleOpenFullScreen = () => setIsFullScreenImage(true);
 
     return (
         <div className={styles.GreetingsWrapper}>
@@ -16,13 +24,25 @@ const Greetings: FC<GreetingsProps> = () => {
                 <RoadIcon/>
             </div>
             <ContentWrapper>
-                <div className={styles.Greetings}>
-                    <img loading='lazy' className={styles.Picture} src={photoPath} alt='кемпер'/>
-                    <div className={styles.GreetingsText}>
-                        <h1 className={styles.Rent}>Аренда автодома для настоящих искателей путешествий</h1>
-                        <div className={styles.Travel}><div className={styles.Description}>Путешествуйте</div><div className={styles.Title}>без границ</div></div>
+                <>
+                    <Modal
+                        className={styles.GalleryModal}
+                        open={isFullScreenImage}
+                        onClose={handleCloseFullScreen}
+                    >
+                        <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }} onClick={handleCloseFullScreen}>
+                            <img src='main.jpg' loading="lazy" alt="фото кемпера"
+                                 className={styles.PictureFullscreen}/>
+                        </Box>
+                    </Modal>
+                    <div className={styles.Greetings}>
+                        <img loading='lazy' className={styles.Picture} src={photoPath} alt='кемпер' onClick={handleOpenFullScreen}/>
+                        <div className={styles.GreetingsText}>
+                            <h1 className={styles.Rent}>Аренда автодома для настоящих искателей путешествий</h1>
+                            <div className={styles.Travel}><div className={styles.Description}>Путешествуйте</div><div className={styles.Title}>без границ</div></div>
+                        </div>
                     </div>
-                </div>
+                </>
             </ContentWrapper>
         </div>
     );
